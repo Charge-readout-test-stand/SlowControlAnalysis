674e733ac62f68b8c5263d8b16f1ef898b3a54da
Brian Lenardo <bglenardo@gmail.com>
2020-02-12 16:11:47 -0800

    Create SCChannelMap.py

---
+++ SCChannelMap.py
@@ -0,0 +1,36 @@
+SCmap = {0: 'time [seconds]',\
+1: 'TC0 Cu Bot [K]',\
+2: 'TC1 Cell Top',\
+3: 'TC2 Cell Mid',\
+4: 'TC3 Cell Bot',\
+5: 'TC4 Cu Top (control TC)',\
+6: 'T_ambient',\
+7: 'LN F/T inlet TC [K]',\
+8: 'LN F/T outlet TC [K]',\
+9: 'Xe recovery bottle [K]',\
+10: 'T_min set point',\
+11: 'T_max set point',\
+12: 'TC15 on valve XV5 [K]',\
+13: 'TC10, regulator temperature [K]',\
+14: 'Omega temp',\
+15: 'extra TC Xe 1',\
+16: 'extra TC Xe 2',\
+17: 'extra TC Xe 3',\
+18: 'LN valve status',\
+19: 'LN enabled',\
+20: 'Heater status',\
+21: 'Mass flow  (uncorrected)',\
+22: 'Pressure from 10k Torr baratron [Torr]',\
+23: 'Pressure from 1k Torr baratron [Torr]',\
+24: 'Cold cathode gauge [micro Torr]',\
+25: 'LN mass (minus hooks and dewar tare weight) [lbs]',\
+26: 'Xenon Bottle mass [kg]',\
+27: 'Capacitance [pF]',\
+28: 'T_max set point offset [K]',\
+29: 'T_min set point offset [K]',\
+30: 'HFE pressure',\
+31: 'LN tare mass (hooks + dewar tare) [lbs]',\
+32: 'Recovery LN valve',\
+33: 'mass flow valve closed',\
+34: 'hanging xenon bottle pressure [psia]',\
+}
